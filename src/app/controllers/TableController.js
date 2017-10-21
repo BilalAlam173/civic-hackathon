93255d40952118296a9e6852463bcc59da29d076
skashanali <kashanali592@gmail.com>
2017-10-22 04:28:23 +0500

    Maps added + add issue and alerts pages ui completed.

--- src/app/controllers/TableController.js
+++ src/app/controllers/TableController.js
@@ -1,23 +1,33 @@
-(function(){
+(function() {
 
-  angular
-    .module('app')
-    .controller('TableController', [
-      'tableService','$rootScope',
-      TableController
-    ]);
+    angular
+        .module('app')
+        .controller('TableController', [
+            'tableService', '$rootScope', 'NgMap', 'issueService', '$rootScope',
+            TableController
+        ]);
 
-  function TableController(tableService,rootScope) {
-    var vm = this;
+    function TableController(tableService, rootScope, ngMap, issueService, rootscope) {
+        var vm = this;
+        vm.tableData = [];
+        var map;
+        console.log(rootScope.userType);
 
-    vm.tableData = [];
-    console.log(rootScope.userType);
+        ngMap.getMap().then(function(map) {
+            console.log(map.getCenter());
+            console.log('markers', map.markers);
+            console.log('shapes', map.shapes);
+        });
 
-    tableService
-      .loadAllItems()
-      .then(function(tableData) {
-        vm.tableData = [].concat(tableData);
-      });
-  }
+        issueService.customRow(JSON.parse(localStorage.getItem('user')).Id).then(function(response) {
+            vm.tableData = response.data;
+            console.log(response);
+            vm.tableData.forEach(function(element, index) {
+                vm.tableData[index].location = [element.lat, element.longit];
+                console.log(vm.tableData[index].location);
+            }, this);
+
+        });
+    }
 
 })();
