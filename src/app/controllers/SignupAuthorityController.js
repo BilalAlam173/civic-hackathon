0fe4908078e8f5e26bc616d006d59af583f5b685
Muhammad Bilal Alam <bilalalam173@gmail.com>
2017-10-21 07:00:45 -0700

    authentication + signup

--- src/app/controllers/SignupAuthorityController.js
+++ src/app/controllers/SignupAuthorityController.js
@@ -3,12 +3,33 @@
     angular
         .module('app')
         .controller('SignupAuthorityController', [
-            '$mdDialog', '$interval',
+            '$mdDialog', '$interval','authorityService','$location',
             SignupAuthorityController
         ]);
 
-    function SignupAuthorityController($mdDialog, $interval) {
+    function SignupAuthorityController($mdDialog, $interval,authorityService,location) {
         var vm = this;
+        vm.signup=signup;
+        vm.user={
+            FullName:"",
+            Email:"",
+            Phone:"",
+            Designation:"",
+            Address:"",
+            DOB:true,
+            Password:""
+        }
+
+        function signup(){
+
+            console.log(vm.user);
+            authorityService.create(vm.user).then(function(response){
+                authorityService.login(vm.user.Email,vm.user.Password).then(function(response){
+                    console.log("login",response);
+                    location.path("/dashboard");
+                });
+            });
+        }
 
     }
 
