aa98c9d1500ae4667d9c8d32fa9a5e5b3f376803
Muhammad Bilal Alam <bilalalam173@gmail.com>
2017-10-21 03:34:51 -0700

    citizen login v1

--- src/app/controllers/LoginController.js
+++ src/app/controllers/LoginController.js
@@ -3,20 +3,32 @@
     angular
         .module('app')
         .controller('LoginController', [
-            '$mdDialog', '$interval', 'citizenService',
+            '$mdDialog', '$interval', 'citizenService','$location',
             LoginController
         ]);
 
-    function LoginController($mdDialog, $interval, citizenService) {
+    function LoginController($mdDialog, $interval, citizenService,location) {
         var vm = this;
         vm.list = list;
+        vm.login=login;
+        vm.type;
+        vm.user={
+            email:'',
+            password:''
+        }
 
         function list() {
-            citizenService.list().then(function(response) {
+            citizenService.auth().then(function(response) {
                 console.log(response);
             });
         }
-        list();
+        function login() {
+            citizenService.login(vm.user).then(function(response) {
+                if(response.data.length>0){
+                    location.path("/dashboard");
+                }
+            });
+        }
 
     }
 
--- src/app/modal/citizen.service.js
+++ src/app/modal/citizen.service.js
@@ -12,12 +12,23 @@
         service.list = function() {
             return $http({
                 method: 'GET',
-                url: 'https://civic-hackathon-server.herokuapp.com/citizen/list',
+                url: 'http://localhost:8090/citizen/list',
                 headers: {
                     'Authorization': 'dmFsbGV5Zm9yZ2U6MTY3NDA='
                 }
             });
         }
+
+        service.login = function(user) {
+            return $http({
+                method: 'GET',
+                url: 'http://localhost:8090/citizen/auth?email='+user.email+'&password='+user.password,
+                headers: {
+                    'Authorization': 'dmFsbGV5Zm9yZ2U6MTY3NDA='
+                }
+            });
+        }
+
         return service;
     }
 
